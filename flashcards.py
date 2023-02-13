cb89b7457424449fccfa903ace42c19cb82941dc
Hartley <bhartley@tristategt.org>
2023-02-13 13:54:27 -0700

    Initial test setup

---
+++ flashcards.py
@@ -0,0 +1,27 @@
+# Core library imports
+pass
+
+# 3rd-party imports
+from flask import Flask
+
+# Local app imports
+pass
+
+
+
+# Flask app name is the filename of this file
+app = Flask(__name__)
+
+
+
+# Home page
+@app.route("/")
+def welcome():
+    from datetime import datetime
+    return "Welcome! Current timestamp: " + str(datetime.now())
+
+
+# About page
+@app.route("/about")
+def about():
+    return "About"
