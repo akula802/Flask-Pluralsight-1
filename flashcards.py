115782c21bc9d43b7bc58858641f15ac460abe3f
Hartley <bhartley@tristategt.org>
2023-02-13 14:48:49 -0700

    End module 3

--- flashcards.py
+++ flashcards.py
@@ -1,11 +1,11 @@
 # Core library imports
-pass
+from datetime import datetime
 
 # 3rd-party imports
-from flask import Flask
+from flask import Flask, render_template
 
 # Local app imports
-pass
+from model import db
 
 
 
@@ -18,11 +18,27 @@ app = Flask(__name__)
 # Home page
 @app.route("/")
 def welcome():
-    from datetime import datetime
-    return "Welcome! Current timestamp: " + str(datetime.now())
+    hello_world_string = "Welcome! Current timestamp: " + str(datetime.now())
+    #return hello_world_string
+    return render_template(
+        "welcome.html",
+        message=hello_world_string,
+    )
+    
 
 
 # About page
 @app.route("/about")
 def about():
     return "About"
+
+
+
+# View to explore the model layer
+@app.route("/card")
+def view_card():
+    card = db[1]
+    return render_template(
+        "view_card.html",
+        card=card,
+    )
